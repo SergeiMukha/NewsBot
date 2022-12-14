812f85a7ca61454306e4693bda375d59a1a26fa9
SergeiMukha <sergeimushonok@example.com>
2022-12-14 03:50:52 +0200

    final architechture

--- src/js/bot.js
+++ src/js/bot.js
@@ -6,29 +6,35 @@ const cheerio = require("cheerio");
 
 const { getHtml, parse } = require("./parseSite");
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+// Create Bot
+const bot = new Telegraf(process.env.BOT_TOKEN_JS)
 
+// Launch Bot
 async function launchBot(lastTitle) {
+    // Get Page Html
     const $ = await getHtml()
 
+    // Parse Html
     const info = await parse($)
 
+    // Form Message Text
     const text = `<b>${info.title}</b>\n\n${info.description.trim()}`
 
+    // Checking if this news is new
     if (info.title != lastTitle) {
+        // Getting News Page To Grab And Parse The Photo in Normal Resolution
         const link = $(".l-list__item").eq(0).find("a.c-card-list__link").attr('href')
 
         res = await got.get(link);
         res = cheerio.load(res.body)
         img = res(".c-post__preview-picture").find("img").attr("src")
 
+        // Sending Message
         bot.telegram.sendPhoto(-1001821022552, img, {
             caption: text,
             parse_mode: "HTML"
         })
     }
-
-    console.log(1)
 
     setTimeout(launchBot, 60000, info.title)
 }
--- src/python/bot.py
+++ src/python/bot.py
@@ -1,27 +1,40 @@
+import os 
 import aiogram
-import asyncio
 import telethon
 from telethon import events
-import os
+from dotenv import load_dotenv, find_dotenv
 
-client = telethon.TelegramClient("session", api_id=14695876, api_hash="3b7a9c4d1c0caa734d13c8985ce40e95")
+load_dotenv(find_dotenv())
 
-bot = aiogram.Bot("5632955841:AAEVBqx1PH8fojYl6PGwBtACi9kGQ6gSTHI")
+BOT_TOKEN = os.getenv("BOT_TOKEN_PYTHON")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+
+# Create Client
+client = telethon.TelegramClient("session", api_id=API_ID, api_hash=API_HASH)
+
+# Create Bot
+bot = aiogram.Bot(BOT_TOKEN)
 dp = aiogram.Dispatcher(bot)
 
+# Start Client
 client.start()
 
+# Tracking For New Messages In Certain Chat
 @client.on(events.NewMessage(chats=('oko_original')))
 async def handler(event):
 
     message = event.message
 
+    # Removing Unnecessary Text
     message.text = message.text.replace("[ПОДПИСАТЬСЯ](https://t.me/+ZU_MUdIOlIs2ZDMy) ✅ [Прислать контент](https://t.me/Info_ukraine_bot)", "")
 
+    # Checking If There Is a Photo or a Video in Message 
     if message.video:
         await client.send_file(entity=-1001685738658, file=message.video, caption=message.text)
     elif message.photo:
         await client.send_file(entity=-1001685738658, file=message.photo, caption=message.text)
-    
+
+# Running Client
 client.run_until_disconnected()
 
